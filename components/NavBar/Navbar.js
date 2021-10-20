652fcf32e8d92db9933a79d210dc0e8dd5e285a4
AnkitRajCode <rajankit432@gmail.com>
2021-10-21 01:37:58 +0530

    repo change

---
+++ components/NavBar/Navbar.js
@@ -0,0 +1,11 @@
+import styles from '../NavBar/Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>Ashish</h1>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/globals.css'
+// import 'tailwindcss/tailwind.css'
+// import '../styles/Home.css'
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
