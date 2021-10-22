381dd829d593802aa038628c3862e231cd88fd3a
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-10-22 12:04:21 +0530

    text change

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
       <div className={head.headerText}>
         <Image src="/img/HeaderText.png" width="435px" height="225px" />
       </div>
-      <div className={head.headerButton}>Discover</div>
+      <div className={head.headerButton}>Open Account</div>
       <div className={head.headerImage}>
 
       <Image src="/img/HeaderImage.png" width="647px" height="346px" />
