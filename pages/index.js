912b8d6de29f008fb8b494a1c1e826f3d96b40a4
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-10-22 10:18:32 +0530

    what we offer section added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import Head from 'next/head';
-import Header from '../components/Header/Header';
+import Head from "next/head";
+import Header from "../components/Header/Header";
 
-import Navbar from '../components/NavBar/Navbar';
+import Navbar from "../components/NavBar/Navbar";
 // import Navbar from "../components/NavBar/Navbar";
 import Card from "../components/Card/card";
-import Whywe from '../components/WhyWe/Whywe';
+import Whywe from "../components/WhyWe/Whywe";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -17,33 +18,32 @@ export default function Home() {
       <Whywe />
 
       {/* Body of Home page */}
-      {/* <div className="mx-auto my-auto text-center">
-        <h1 className="text-3xl md:text-4xl text-purple-900 font-bold">
-          What Do we Offer ?
-        </h1>
-      </div>
+      <div className="py-10">
+        <div className="mx-auto text-center mt-10 lg:text-4xl text-purple-900 font-bold mb-5">
+          <p>What we Offer !</p>
+        </div>
+        <div className="absolute ml-5 z-10">
+        <Image src = "/img/art.png" height={229} width={231}/>
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-3 lg:gap-x-px justify-items-center rounded-lg">
 
-      <section className="md:h-full flex items-center text-gray-600 mt-12">
-      <div className="mx-auto my-auto flex gap-x-7 ">
-        <Card
-          title="Stock"
-          imgURL="/img/stock.png"
-          description="Invest in thousands of companies and fractional shares with as little as $5 using our trading tools and analytics to create your own financial portfolio."
-        />
-        <Card
-          title="Crypto"
-          imgURL="/img/crypto.png"
-          description="nvest in thousands of companies and fractional shares with as little as $5 using our trading tools and analytics to create your own financial portfolio."
-        />
-        <Card
-          title="Insurance"
-          imgURL="/img/insurance.png"
-          description="nvest in thousands of companies and fractional shares with as little as $5 using our trading tools and analytics to create your own financial portfolio."
-        />
+          <Card
+            title="Stock"
+            imgURL="/img/stock.png"
+            description="Invest in thousands of companies and fractional shares with as little as $5 using our trading tools and analytics to create your own financial portfolio."
+          />
+          <Card
+            title="Crypto"
+            imgURL="/img/crypto.png"
+            description="Invest in thousands of companies and fractional shares with as little as $5 using our trading tools and analytics to create your own financial portfolio."
+          />
+          <Card
+            title="Insurance"
+            imgURL="/img/insurance.png"
+            description="Invest in thousands of companies and fractional shares with as little as $5 using our trading tools and analytics to create your own financial portfolio."
+          />
+        </div>
       </div>
-      </section>    */}
-      {/* <Header/>     */}
-
     </div>
   );
 }
