92e8d0a15ab23fb8fc7f0ec01cf137ed3df49932
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-10-24 17:37:39 +0530

    Header-animations

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,6 +1,29 @@
 import head from './Header.module.css';
 import Image from "next/image";
-
+import { motion } from "framer-motion";
+let easing = [0.6, -0.05, 0.01, 0.99];
+const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0,
+    transition: { duration: 0.6, ease: easing }
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: easing
+    }
+  }
+};
+const stagger = {
+  animate: {
+    transition: {
+      staggerChildren: 0.1
+    }
+  }
+};
 const Header = () => {
   return (
     <div className={head.header}>
@@ -14,9 +37,12 @@ const Header = () => {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dignissim tempus ligula, sed egestas libero pharetra eget.
       </div>
       <div className={head.headerButton}>Open Account</div>
-      <div className={head.headerImage}>
+      <motion.div variants={stagger} className={head.headerImage}>
+        <motion.div variants={fadeInUp} whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}>
         <Image src="/img/HeaderImage.png" width="650px" height="427px" />
-      </div>
+        </motion.div>        
+      </motion.div>
     </div>
   )
 }
