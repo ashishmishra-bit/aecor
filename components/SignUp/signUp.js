a18e962f6a399dc0bc4dfe5f5b043959ca015385
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-10-29 08:51:46 +0530

    signUP page

--- components/SignUp/signUp.js
+++ components/SignUp/signUp.js
@@ -92,7 +92,7 @@ const signUp = () => {
       </div>
 
       {/* Modal */}
-      {/* {showModal ? (
+      {showModal ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
@@ -195,7 +195,7 @@ const signUp = () => {
           </div>
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
         </>
-      ) : null} */}
+      ) : null}
 
       {/* Terms & Condition section */}
 
