26e1694d4158030a0ef6c23d9b970e9d271b7724
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-10-30 18:07:03 +0530

    refactor

--- components/WhyWe/We.js
+++ components/WhyWe/We.js
@@ -30,66 +30,7 @@ const We = () => {
     });
   }, []);
 
-  return (
-    // <div className=" overflow-x-hidden">
-    //     <div className="absolute lg:mt-60 lg:ml-10 mt-10 ml-5">
-    //     <Image src = "/img/art.png" height={229} width={231}/>
-    //     </div>
-    //   <div className="container flex flex-col lg:flex-row items-center gap-12 mt-14 lg:mt-28 ml-28 -ml-8">
-    //     {/* content */}
-        
-    //     <div className="flex flex-1 flex-col items-center lg:items-start  md:ml-10 ml-10 lg:ml-32">
-        
-    //       <h2 className="text-purple-900 text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6 font-bold z-10">
-    //         Why We Are <br />
-    //         Better Than Others
-    //       </h2>
-          
-    //       <p className="text-bookmark-grey text-lg  lg:text-left mb-6 break-words text-gray-500 text-justify lg:w-5/6 ml-5 lg:ml-0">
-    //         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-    //         dignissim tempus ligula, sedlyf egestas libero pharetra
-    //         eget.Phasellus dignissim tempus ligula, sedlyf egestas libero
-    //         pharetra eget. Phasellus dignissim tempus ligula, sed egestas libero
-    //         pharetra eget.
-    //       </p>
-    //     </div>
-    //     {/* image */}
-    //     <div className="flex-col justify-center flex-1 mb-10 md:mb-16 lg:mb-0 lg:ml-60 ml-16">
-    //     <animated.div className = "cursor-pointer" onMouseMove={({clientX: x, clientY: y}) => (set({xys: calc(x, y)}))}
-    //         onMouseLeave={() => set({xys:[0,0,1]})}
-    //         style={{
-    //             transform: props.xys.interpolate(trans)
-    //         }}>
-    //         <Image src="/img/ios.png" width={251} height={358} />
-    //       </animated.div>
-
-    //       <animated.div  className = "cursor-pointer" onMouseMove={({clientX: x, clientY: y}) => (set({xys: calc(x, y)}))}
-    //         onMouseLeave={() => set({xys:[0,0,1]})}
-    //         style={{
-    //             transform: props.xys.interpolate(trans)}}
-    //       >
-    //         <Image src="/img/android.png" width={251} height={358} />
-    //       </animated.div>
-    //     </div>
-    //     <animated.div className="lg:-ml-52 flex-col justify-center flex-1 mb-10 md:mb-16 lg:mb-0 cursor-pointer ml-16 -mt-20"
-    //     onMouseMove={({clientX: x, clientY: y}) => (set({xys: calc(x, y)}))}
-    //     onMouseLeave={() => set({xys:[0,0,1]})}
-    //     style={{
-    //         transform: props.xys.interpolate(trans)}}
-      
-    //     >
-    //       <Image src="/img/web.png" width={251} height={358} />
-    //     </animated.div>
-    //   </div>
-
-    //   <style jsx>{`
-    //     .container {
-    //         // margin-left:70px;
-    //         padding-top: 50px;
-    //         padding-bottom: 50px;
-    //     }
-    //   `}</style>
-    // </div>
+  return ( 
 
     <> 
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
@@ -103,7 +44,7 @@ const We = () => {
               <span className="relative px-1">
                 <div className="absolute inset-x-0 bottom-0 h-3 transform -skew-x-12 bg-teal-accent-400" />
                 <span className="relative inline-block text-purple-800">
-                  "Financial Sector"
+                  Financial Sector
                 </span>
               </span>
             </h2>
