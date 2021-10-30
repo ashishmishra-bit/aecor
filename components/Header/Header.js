20cf24a88da2d02b92c9eaf77d9ff25e516681b3
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-10-30 18:41:41 +0530

    refactor

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -69,7 +69,7 @@ const Header = () => {
                 Open Account
               </a>
               <a
-                href="/about"
+                href="#services"
                 aria-label=""
                 className="inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700"
               >
