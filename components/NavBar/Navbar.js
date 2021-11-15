1fd7d4f9ecb6d808fd8e70f7268e60a0ed14492b
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-11-15 09:40:23 +0530

    nav links

--- components/NavBar/Navbar.js
+++ components/NavBar/Navbar.js
@@ -58,7 +58,7 @@ export const Navbar = () => {
               </a>
             </Link>
 
-            <Link href="#">
+            <Link href="/product">
               <a className="lg:inline-flex lg:w-auto w-full px-5 py-2 rounded text-purple-800 font-bold items-center justify-center hover:border-purple-800 hover:border-b-2 ">
                 Product
               </a>
